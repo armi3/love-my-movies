585b1e4da70227b3ea0df28ef9c99a12c5068437
armi3 <fernandagonzalez@ufm.edu>
2019-11-07 23:41:32 -0600

    eggos y bacon pls

--- application/requester.py
+++ application/requester.py
@@ -2,10 +2,29 @@ import requests
 
 
 @staticmethod
-def get_list(app, call):
+def get_info(app, db, list_id):
+    # check for list in redis cache
+    if db.exists(list_id):
+        # next refactor: check for changes/updates here
+        return db.get(list_id)
+    else:
+        # if not cached, request from API
+        json = get_from_api(app, list_id)
+        set_from_json(json)
+        return db.get(list_id)
+
+
+def get_from_api(app, list_id):
+    call = 'https://api.themoviedb.org/3/list/' + list_id + '?api_key='
     url = call + app.config['API_KEY']
     r = requests.get(url)
     print(r.status_code)
     print(r.json())
     print(type(r.json()))
     return r.json()
+
+
+def set_from_json(db, json, list_id):
+    db.set(list_id, json['name'])                               # name
+    db.set(list_id+'item_count', json['item_count'])            # item_count
+    db.set(list_id + 'item_count', json['item_count'])
--- application/routes.py
+++ application/routes.py
@@ -15,17 +15,17 @@ def home():
 
 @app.route("/list1")
 def list1():
-    info = requester.get_list.__func__(app, 'https://api.themoviedb.org/3/list/124389?api_key=')
+    info = requester.get_info.__func__(app, db, '124389')
     return render_template("list.html", info=info)
 
 
 @app.route("/list2")
 def list2():
-    info = requester.get_list.__func__(app, 'https://api.themoviedb.org/3/list/124464?api_key=')
+    info = requester.get_info.__func__(app, db, '124464')
     return render_template("list.html", info=info)
 
 
 @app.route("/list3")
 def list3():
-    info = requester.get_list.__func__(app, 'https://api.themoviedb.org/3/list/124465?api_key=')
+    info = requester.get_info.__func__(app, db, '124465')
     return render_template("list.html", info=info)
